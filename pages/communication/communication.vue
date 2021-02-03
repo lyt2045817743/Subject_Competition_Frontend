ead431148cafcd68d49769899a147ecf57554ff7
lyt2045817743 <2045817743@qq.com>
2021-02-03 18:13:24 +0800

    新建前端项目

---
+++ pages/communication/communication.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		交流
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/competition/competition.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		竞赛
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
